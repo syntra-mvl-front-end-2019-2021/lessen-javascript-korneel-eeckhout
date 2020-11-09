3faabf4022f3e3702a17ee0059718c7b5d3aa742
Korneel Eeckhout <korneel@pienter.tech>
2020-11-09 10:47:53 +0100

    Rock paper scissors solution Mastermind tips

--- les-2/mastermind/main.js
+++ les-2/mastermind/main.js
@@ -2,9 +2,30 @@ const masterMind = function () {
     // generate an array with 5 random number between 0 and 5
     // ask for 5 numbers between 0 and 5
     // insert them into an array
-    // count the number of numbers which are the same but do not have the same postion
-    // count the number of numbers which are the same and have the same position
+    // count the number of numbers which are the same but do not have the same position
+    // count the number of numbers which are the same and have the same position for (i=0; i<5; i++)
     // alert both to the player
     // if all the numbers are correct the player wins
     // if not let the player try again
+    // max 15 guesses
 };
+
+// for (let i=0; i<5; i++) {
+//     const solutionVal = solution[i];
+//     for (let j=0; j<5; j++) {
+//         const guessVal = guess[j];
+//
+//         if (solutionVal === guessVal) {
+//
+//         }
+//     }
+// }
+
+// Math.min(1,2); -> returns 1
+
+// [1,1,0,4,5]
+// [4,0,0,1,1]
+// solution: 0 -> 1 | 1 -> 2 | 2 -> 0 | 3 -> 0 | 4 -> 1 | 5 -> 1
+// guess: 0 -> 2 | 1 -> 2 | 2 -> 0 | 3 -> 0 | 4 -> 1 | 5 -> 0
+// Sum min values: 1 + 2 + 0 + 1 + 0 = 4
+// Subtract correct number and place count = 4 - 1 -> 4
--- les-2/rock-paper-scissors/main.js
+++ les-2/rock-paper-scissors/main.js
@@ -1,15 +1,90 @@
+const arrayRPS = ['rock', 'paper', 'scissors'];
+
+const promptInt = function (question, minVal, maxVal) {
+    let answer = parseInt(prompt(question));
+
+    while (isNaN(answer) || answer < minVal || answer > maxVal) {
+        answer = parseInt(prompt('Insert a valid number: \n' + question));
+    }
+
+    return answer;
+};
+
+const getComputerGuess = function () {
+    let randomIndex = Math.floor(Math.random() * 3);
+    return arrayRPS[randomIndex];
+};
+
+const getPlayerGuess = function () {
+    let playerGuess = prompt('rock, paper, scissors?');
+
+    while (
+        // playerGuess.toLowerCase() !== 'rock' &&
+        // playerGuess.toLowerCase() !== 'paper' &&
+        // playerGuess.toLowerCase() !== 'scissors'
+
+        !arrayRPS.includes(playerGuess.toLowerCase().trim())
+    ) {
+        playerGuess = prompt(
+            'Select a correct value: \n rock, paper, scissors?',
+        );
+    }
+
+    return playerGuess.toLowerCase().trim();
+};
+
 const rockPaperScissors = function () {
-    // generate random rock/paper/scissors
-    // ask for rock/paper/scissors
-    // make sure value is correct
-    // determine winner
-    // alert winner
+    let computerGuess = getComputerGuess();
+    let playerGuess = getPlayerGuess();
+
+    while (computerGuess === playerGuess) {
+        alert('AI chose ' + computerGuess + "\nIt's a tie, try again");
+        computerGuess = getComputerGuess();
+        playerGuess = getPlayerGuess();
+    }
+
+    if (
+        (computerGuess === arrayRPS[0] && playerGuess === arrayRPS[1]) ||
+        (computerGuess === arrayRPS[1] && playerGuess === arrayRPS[2]) ||
+        (computerGuess === arrayRPS[2] && playerGuess === arrayRPS[0])
+    ) {
+        alert('AI chose ' + computerGuess + '\nYou won');
+        return true;
+    } else {
+        alert('AI chose ' + computerGuess + '\nYou lost');
+        return false;
+    }
 };
 
 const rockPaperScissorsCompetition = function () {
-    // ask how many games the player wants to play
-    // play rockPaperScissors() the correct number of time
-    //  if the number of games is 5 and the player wins 3 games no 4th game should be played
-    // keep and alert score
-    // alert winner
+    let maxGames = promptInt(
+        'How many games do you want to play?',
+        1,
+        Infinity,
+    );
+    let playerScore = 0;
+    let aiScore = 0;
+
+    while (
+        playerScore * 2 < maxGames &&
+        aiScore * 2 < maxGames &&
+        playerScore + aiScore < maxGames
+    ) {
+        if (rockPaperScissors()) {
+            playerScore++;
+        } else {
+            aiScore++;
+        }
+        alert('Current score: \nAI: ' + aiScore + '\nPlayer: ' + playerScore);
+    }
+
+    if (playerScore > aiScore) {
+        alert('You win the competition!');
+    } else if (playerScore < aiScore) {
+        alert('You lost the competition!');
+    } else {
+        alert('Nobody wins, play again?');
+    }
 };
+
+rockPaperScissorsCompetition();
