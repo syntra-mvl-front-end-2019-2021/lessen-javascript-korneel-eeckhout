6f368e4e41a9cc360976404be2dadf7747f6d470
Korneel Eeckhout <korneel@pienter.tech>
2020-11-09 12:41:20 +0100

    Intro DOM

---
+++ les-3/main.js
@@ -0,0 +1,28 @@
+// console.log(document.body);
+// console.log(document.body.childNodes);
+// console.log(document.body.children);
+// console.log(document.body.childNodes[0]);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+
+// const bodyChildren = document.body.childNodes;
+//
+// console.log(bodyChildren);
+//
+// bodyChildren[1].innerText = 'test';
+//
+// console.log(bodyChildren);
+
+// console.log(document.body.parentNode);
+//
+// const pTag = document.body.childNodes[1];
+//
+// console.log(pTag.nextSibling);
+
+console.log(document.getElementById('select-me-3'));
+console.log(document.getElementsByClassName('select-me-too-2'));
+console.log(document.getElementsByTagName('p'));
+console.log(document.getElementsByName('input'));
+
+console.log(document.querySelector('.select-me-too:nth-child(2)'));
+console.log(document.querySelectorAll('.select-me-too'));
