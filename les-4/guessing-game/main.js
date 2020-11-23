3e78428d96b337728bd395c0b73711956607ed16
Korneel Eeckhout <korneel@pienter.tech>
2020-11-23 10:53:28 +0100

    Guessing game v2

--- les-4/guessing-game/main.js
+++ les-4/guessing-game/main.js
@@ -22,3 +22,125 @@
 //    > do not add new p
 // 9 max 5 guesses
 //10 keep guess count in #guess-count
+
+const $solution = document.getElementById('solution');
+const $guessSubmit = document.getElementById('guess-submit');
+const $guessInput = document.getElementById('guess-input');
+const $guessContainer = document.getElementById('guess-container');
+const $guessCount = document.getElementById('guess-count');
+const $message = document.getElementById('message');
+
+let guessCount = 0;
+let minVal = 0;
+let maxVal = 10;
+let solution;
+let gameDone = false;
+
+function genRandomNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function printSolution(solution) {
+  $solution.textContent = solution;
+}
+
+function hideSolution() {
+  $solution.classList.add('hidden');
+}
+
+function showSolution() {
+  $solution.classList.remove('hidden');
+}
+
+// function showHideSolution(hide) {
+//   if (hide) {
+//     $solution.classList.add('hidden');
+//   } else {
+//     $solution.classList.remove('hidden');
+//   }
+// }
+
+function getGuessInput() {
+  let answer = parseInt($guessInput.value);
+
+  if (isNaN(answer) || answer < minVal || answer > maxVal) {
+    return false;
+  } else {
+    return answer;
+  }
+}
+
+function printGuess(guess) {
+  // $guessContainer.innerHTML += '<p class="guess">' + guess + '</p>';
+
+  // const $newGuess = document.createElement('p');
+  // $newGuess.textContent = guess;
+  // $guessContainer.append($newGuess);
+
+  $guessContainer.insertAdjacentHTML('beforeend', '<p class="guess">' + guess + '</p>');
+  printGuessCount();
+}
+
+function printGuessCount() {
+  $guessCount.textContent = guessCount;
+}
+
+function printMessage(message) {
+  $message.textContent = message;
+}
+
+function setGameDone(done) {
+  if (done) {
+    $guessSubmit.textContent = 'Restart';
+    gameDone = true;
+  } else {
+    $guessSubmit.textContent = 'Guess';
+    gameDone = false;
+  }
+}
+
+function clearGuesses() {
+  $guessContainer.innerHTML = '';
+  $guessInput.value = '';
+  guessCount = 0;
+  printGuessCount();
+}
+
+function initGuessingGame() {
+  solution = genRandomNumber(maxVal);
+  hideSolution();
+  printSolution(solution);
+  setGameDone(false);
+  clearGuesses();
+  printMessage('');
+}
+
+function submitGuess() {
+  if (gameDone) {
+    initGuessingGame();
+  } else {
+    const guess = getGuessInput();
+
+    if (guess === false) {
+      printMessage('Guess a valid number between ' + minVal + ' and ' + maxVal);
+    } else {
+      guessCount++;
+      printGuess(guess);
+
+      if (guess === solution) {
+        printMessage('You won');
+        showSolution();
+        setGameDone(true);
+      } else if (guessCount === 5) {
+        showSolution();
+        setGameDone(true);
+        printMessage('You lost, restart?')
+      } else {
+        printMessage('Not correct,try again');
+      }
+    }
+  }
+}
+
+initGuessingGame();
+$guessSubmit.addEventListener('click', submitGuess)
