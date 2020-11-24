bda5b183012b57adca2897b15100231a881394ec
Korneel Eeckhout <korneel@pienter.tech>
2020-11-24 13:53:59 +0100

    Rock paper scissors

---
+++ les-5/rock-paper-scissors/main.js
@@ -0,0 +1,16 @@
+// player clicks button
+// > Determine players choice
+// > Generate ai choice
+// > Compare
+// >> Determine winner/tie
+// >>> Update counts
+// >>> Add history line
+// >>>> Append p tag in #history-container
+// >>> Update message
+
+// Fase 2
+// Add restart btn
+// > On click
+// >> Empty #history-container
+// >> Reset counts
+// >> Empty message
